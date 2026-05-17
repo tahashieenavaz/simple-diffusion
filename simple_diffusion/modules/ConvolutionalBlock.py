7e65a6fb19cd57a2ac9eb841dbbd60e9baa604f9
tahashieenavaz <tahashieenavaz@gmail.com>
2026-05-17 18:18:45 +0200

    wip

--- simple_diffusion/modules/ConvolutionalBlock.py
+++ simple_diffusion/modules/ConvolutionalBlock.py
@@ -1,22 +1,29 @@
 import torch
+from typing import Type
 
 
 class ConvolutionalBlock(torch.nn.Module):
-    def __init__(self, in_channels: int, out_channels: int, time_dimension: int):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        time_dimension: int,
+        activation: Type[torch.nn.Module] = torch.nn.GELU,
+    ):
         super().__init__()
-        self.conv1 = torch.nn.Conv2d(
-            in_channels, out_channels, kernel_size=3, padding=1
-        )
-        self.conv2 = torch.nn.Conv2d(
+        self.abel = torch.nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.cain = torch.nn.Conv2d(
             out_channels, out_channels, kernel_size=3, padding=1
         )
-        self.activation = torch.nn.GELU()
+        self.activation = activation()
         self.time_mlp = torch.nn.Linear(time_dimension, out_channels)
 
     def forward(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
-        x = self.activation(self.conv1(x))
-        time_emb = self.activation(self.time_mlp(t))
-        time_emb = time_emb.view(time_emb.size(0), time_emb.size(1), 1, 1)
-        x = x + time_emb
-        x = self.activation(self.conv2(x))
+        x = self.activation(self.abel(x))
+        time_embedding = self.activation(self.time_mlp(t))
+        time_embedding = time_embedding.view(
+            time_embedding.size(0), time_embedding.size(1), 1, 1
+        )
+        x = x + time_embedding
+        x = self.activation(self.cain(x))
         return x
